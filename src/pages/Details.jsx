8a565b16c8827b37d388f5a76b3bf2af2632fe2c
Ali Anani <aliananii120@gmail.com>
2024-10-30 16:32:59 +0300

    Adjusted Border countries

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -16,7 +16,7 @@ const Details = () => {
         <div>
             <img className='rounded-lg lg:w-[540px]' src={data[0].flags.svg} alt="" />
         </div>
-        <div className='w-3/6'>
+        <div className='lg:w-3/6'>
             <div className='my-6'>
                 <h1 className='font-extrabold text-2xl'>Belgium</h1>
             </div>
@@ -28,13 +28,13 @@ const Details = () => {
                     <p className='font-light'><span className='font-semibold'>Native Name:{' '}</span>Belgie</p>
                     <p className='font-light'><span className='font-semibold'>Native Name:{' '}</span>Belgie</p>
                 </div>
-                <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-2 lg:w-3/6'>
                     <p className='font-light'><span className='font-semibold'>Native Name:{' '}</span>Belgie</p>
                     <p className='font-light'><span className='font-semibold'>Native Name:{' '}</span>Belgie</p>
                     <p className='font-light'><span className='font-semibold'>Native Name:{' '}</span>Belgie</p>
                 </div>         
             </div>
-            <div className='items-center lg:flex lg:gap-10 lg:mt-10'>
+            <div className='items-center lg:flex lg:gap-10 mt-10'>
                 <h1 className='font-semibold text-lg mb-4 lg:mb-0'>Border Countries:</h1>
                 <div className='flex flex-wrap gap-2'>
                     <span className='flex justify-center items-center py-2 w-[7rem] box-shadow rounded-sm text-sm dark:bg-[#2B3844]'>France</span>
